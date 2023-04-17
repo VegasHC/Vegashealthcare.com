af3a31eb5f14a1d5440daee2e534fb258ca91e91
edalzell <erin@silentz.co>
2023-04-17 14:42:23 -0700

    put back config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-    content: ['./public/**/*.{html,js}', './resources/**/*.{html,js,vue}'],
+    content: [
+        './public/**/*.{html,js}',
+        './resources/**/*.{html,js}',
+        './resources/**/*.antlers.php',
+        './resources/**/*.blade.php',
+        './content/**/*.md'
+    ],
+    important: true,
+    theme: {
+        extend: {
+            colors: {
+                primary: "var(--color-primary)",
+                secondary: "var(--color-secondary)",
+                brand: "var(--color-brand)",
+                heading: "var(--color-heading)",
+                "bg-dark": "var(--color-background-dark)",
+                "bg-light": "var(--color-background-light)"
+            },
+            opacity: {
+                '01': '.001',
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 }
