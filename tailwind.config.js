e32c0eba7d3a54e0cafc4dfd908c20d6ba58f36b
Shift <shift@laravelshift.com>
2023-04-17 21:05:27 +0000

    Configure JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,3 @@
 module.exports = {
-    purge: {
-      content: [
-         './resources/**/*.antlers.html',
-          './resources/**/*.antlers.php',
-          './resources/**/search.js',
-          './resources/**/*.blade.php',
-          './content/**/*.md'
-      ]
-    },
-    important: true,
-    theme: {
-        extend: {
-            colors: {
-                primary: "var(--color-primary)",
-                secondary: "var(--color-secondary)",
-                brand: "var(--color-brand)",
-                heading: "var(--color-heading)",
-                "bg-dark": "var(--color-background-dark)",
-                "bg-light": "var(--color-background-light)"
-            },
-            opacity: {
-                '01': '.001',
-            }
-        },
-
-
-    },
-    variants: {},
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-    ],
-  }
+    content: ['./public/**/*.{html,js}', './resources/**/*.{html,js,vue}'],
+}
